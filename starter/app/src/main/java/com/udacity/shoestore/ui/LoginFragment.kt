91dd56155d75faf1987aeacad8ec5ec248ff5ea5
Arthur Paulucci Carnieto <arthurcarnieto@gmail.com>
2025-04-13 14:21:51 -0300

    Add LoginFragment
    And also adapt main activity layout to use it

---
+++ starter/app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.udacity.shoestore.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.udacity.shoestore.R
+import timber.log.Timber
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Timber.d("onCreateView")
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}

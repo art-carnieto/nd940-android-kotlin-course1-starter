e515d8967256b9ab9e04e82739878ff893245c1f
Arthur Paulucci Carnieto <arthurcarnieto@gmail.com>
2026-01-25 19:42:44 -0300

    fix(shoeList): Fix the shoe list preloaded list
    The biggest difference is that now the list is loaded on populateShoes
    of view model class, and injected on the layout one by one on a for
    loop.
    
    This required changing the image type of variable of Shoe class (it's
    now a resource integer, which makes way more sense) and also creating
    a separate layout for a shoe item, just to make it easier to inflate
    them on inflateShoes of ShoeListFragment.
    
    Also fix minor warnings of missing some content description strings
    and deprecated packages.

--- starter/app/src/main/java/com/udacity/shoestore/models/Shoe.kt
+++ starter/app/src/main/java/com/udacity/shoestore/models/Shoe.kt
@@ -1,8 +1,9 @@
 package com.udacity.shoestore.models
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import androidx.annotation.DrawableRes
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Shoe(var name: String, var size: Double, var company: String, var description: String,
-                val images: List<String> = mutableListOf()) : Parcelable
+                @DrawableRes val imgResourceId: Int) : Parcelable
